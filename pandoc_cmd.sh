7f2d0b96f4e72b5c165c46a07337f60f7212fb90
Dominik Neise <neised@phys.ethz.ch>
2016-07-04 07:10:02 +0200

    initial commit of bash script to convert README.md to report.pdf

---
+++ pandoc_cmd.sh
@@ -0,0 +1,8 @@
+pandoc \
+--variable papersize=A4 \
+--variable margin-left=2cm \
+--variable margin-right=2cm \
+--variable margin-top=2cm \
+--variable margin-bottom=2cm \
+--variable colorlinks \
+README.md -o report.pdf
