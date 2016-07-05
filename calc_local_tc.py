8f51557b51fa8f9c2c8f0885f05c25e9d0ab05a0
Dominik Neise <neised@phys.ethz.ch>
2016-07-05 10:14:57 +0200

    should be okay now

--- calc_local_tc.py
+++ calc_local_tc.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Usage:
   calc_local_tc.py [options]
@@ -15,34 +16,51 @@ import numpy as np
 from tqdm import tqdm
 from docopt import docopt
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def calc_local_tc(event_generator, calib, pixel, gain):
     all_slopes = [[] for i in range(1024)]
+    stop_cells = np.zeros(1024, dtype=int)
 
     for event in tqdm(event_generator):
         event = calib(event)
         calibrated = event.data[pixel][gain]
         
         zero_crossings = np.where(np.diff(np.signbit(calibrated)))[0]
-        slope = calibrated[zero_crossings + 1] - calibrated[zero_crossings]
+        slopes = calibrated[zero_crossings + 1] - calibrated[zero_crossings]
+        absolute_slopes = np.abs(slopes)
 
-        zero_crossing_cells = dr.sample2cell(zero_crossings, 
-            stop_cell=event.header.stop_cells[pixel][gain], 
+        sc = event.header.stop_cells[pixel][gain]
+        stop_cells[sc%1024] += 1
+        zero_crossing_cells = dr.sample2cell(zero_crossings+1, 
+            stop_cell=sc,
             total_cells=1024)
         
-        for crossing_number, cell_id in enumerate(zero_crossing_cells):
-            all_slopes[cell_id].append(np.abs(slope[crossing_number]))
+        for abs_slope, cell_id in zip(absolute_slopes, zero_crossing_cells):
+            all_slopes[cell_id].append(abs_slope)
 
     tc = pd.DataFrame({
             "cell_width_mean": np.zeros(len(all_slopes), dtype=np.float32),
             "cell_width_std": np.zeros(len(all_slopes), dtype=np.float32),
             "number_of_crossings": np.zeros(len(all_slopes), dtype=np.int32),
-        })    
+            "stop_cell": stop_cells,
+            "slope_mean": np.zeros(len(all_slopes), dtype=np.float32),
+        })
     for cell_id, slopes in enumerate(all_slopes):
         slopes = np.array(slopes)
         tc.loc[cell_id, "number_of_crossings"] = len(slopes)
-        tc.loc[cell_id, "cell_width_mean"] = slopes.mean()
+        tc.loc[cell_id, "cell_width_mean"] = np.mean(slopes) # np.median(slopes)
+        tc.loc[cell_id, "slope_mean"] = slopes.mean()
+        if False:
+            plt.hist(slopes, bins=50, histtype="step")
+            plt.title(str(len(slopes)))
+            ax = plt.gca()
+            ax.ticklabel_format(useOffset=False)
+            plt.savefig("slopes/{0:04d}.png".format(cell_id))
+            plt.close("all")
+        
         tc.loc[cell_id, "cell_width_std"] = slopes.std() / np.sqrt(len(slopes))
+
 
     average_of_all_slopes = tc.cell_width_mean.dropna().mean()
     tc["cell_width_mean"] /= average_of_all_slopes
@@ -62,7 +80,13 @@ if __name__ == "__main__":
         calib = dr.calibration.TakaOffsetCalibration(args["-c"])
     else:
         from fake_event_gen import FakeEventGenerator
-        event_generator = FakeEventGenerator(trigger_times=np.random.uniform(0, 1e-8, 10000).cumsum())
+        event_generator = FakeEventGenerator(
+            trigger_times=np.arange(10000)* (1/300), # 50k evts at 300Hz
+            #trigger_times=np.arange(10000)* (1/300 * (1 + 0e-6)), # 50k evts at 300Hz
+            #trigger_times=np.random.uniform(0,1e-9, 10000).cumsum(),
+            random_phase=False,
+            sine_frequency=30e6 * (1+1e-7),
+        )
         calib = lambda x: x
 
     tc = calc_local_tc(event_generator, calib, pixel, gain)
